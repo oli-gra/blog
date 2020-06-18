fdb174dc0a775769877a7330f33231df38de3d72
oli-gra <oliver.grant@gmail.com>
2020-06-18 11:24:49 +0100

    context

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native'
+
+const IndexScreen = () => {
+  return <>
+  </>
+}
+
+const styles = StyleSheet.create()
+
+export default IndexScreen
